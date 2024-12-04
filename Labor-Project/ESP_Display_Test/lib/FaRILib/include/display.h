003cbb96208de333f6b1e8fbbd76f01815799711
Fabian Schaetzschock <fabian.schaetzschock@gmail.com>
2024-12-04 13:25:50 +0100

    fixed issue where states could not be toggled

--- Labor-Project/ESP_Display_Test/lib/FaRILib/include/display.h
+++ Labor-Project/ESP_Display_Test/lib/FaRILib/include/display.h
@@ -5,12 +5,13 @@
 #include <ui.h>
 
 // MOSI=23, MISO=19, SCK=18
-#define CS_PIN  10
+// #define CS_PIN  10
 
-static const uint16_t screenWidth  = 800;
-static const uint16_t screenHeight = 480;
+static const uint16_t displayWidth  = 800;
+static const uint16_t displayHeight = 480;
 static lv_disp_draw_buf_t draw_buf;
-static lv_color_t buf[ screenWidth * screenHeight / 10 ];
+static lv_color_t buf[ displayWidth * displayHeight / 10 ];
 
 void my_disp_flush(lv_disp_drv_t *disp, const lv_area_t *area, lv_color_t *color_p);
+//initialize all display functionality (tft_eSPI, lvgl, SquareLineStudio)
 void initTFT(TFT_eSPI *tft);
