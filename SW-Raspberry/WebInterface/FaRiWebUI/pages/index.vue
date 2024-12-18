0edbdd936cca672951ec6c9d2ba82ad0837c3f20
Fabian Schaetzschock <fabian.schaetzschock@gmail.com>
2024-12-18 15:47:39 +0100

    incomplete SerialMonitor for esp32-c3

--- SW-Raspberry/WebInterface/FaRiWebUI/pages/index.vue
+++ SW-Raspberry/WebInterface/FaRiWebUI/pages/index.vue
@@ -1,9 +1,11 @@
 <template>
     <div id="Page">
         <h1>Hello World</h1>
+        <p v-if="error">Error: {{ error.message }}</p>
+        <p v-else>Data: {{ ledState }}</p>
     </div>
 </template>
 
-<!-- <style>
-    
-</style> -->
+<script setup lang="ts">
+    const { data: ledState, error } = await useFetch('http://192.168.10.84/fetch')
+</script>
