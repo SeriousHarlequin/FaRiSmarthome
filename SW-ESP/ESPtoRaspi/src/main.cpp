e6eb66a3308bc870eef39ad20aecb80783aa094f
Fabian Schaetzschock <fabian.schaetzschock@gmail.com>
2025-01-08 07:38:29 +0100

    Add .gitignore files, update networking chapter, and implement WebServer functionality

---
+++ SW-ESP/ESPtoRaspi/src/main.cpp
@@ -0,0 +1,55 @@
+#include <Arduino.h>
+#include <OneWire.h>
+#include <DallasTemperature.h>
+#include "WebServer.h"
+#define TEMP_SENSOR 0
+
+OneWire oneWire(TEMP_SENSOR);
+float temperatureC;
+bool measureTemp = false;
+
+// Pass our oneWire reference to Dallas Temperature sensor 
+DallasTemperature sensors(&oneWire);
+
+AsyncWebServer server(80);
+
+void setup() {
+  Serial.begin(115200);
+  delay(1000);
+  initISR();
+  initWebServer(&server);
+
+  sensors.begin();
+  Serial.println("Temperature Sensor is ready");
+}
+
+void loop() {
+  ISRHandler();
+  delay(100);
+}
+
+void isr(){//0.1ms
+  static unsigned int count;
+
+  if(count == 10000){//1s
+    count = 0;
+    measureTemp = true;
+  }
+
+  count++;  
+}
+
+ 
+void ISRHandler(){
+  if(measureTemp){//every one second
+    measureTemp = false;
+    sensors.requestTemperatures(); 
+    temperatureC = sensors.getTempCByIndex(0);
+    // Serial.print("Temperature: ");
+    // Serial.println(temperatureC);
+
+  }
+
+}
+
+
