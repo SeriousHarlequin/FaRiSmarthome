25d99f8889568bc3b2f1b4fcf837ac8f565f8770
Fabian Schaetzschock <fabian.schaetzschock@gmail.com>
2025-01-29 16:00:34 +0100

    Add initial Ethernet support and project structure for UDP testing

---
+++ SW-ESP/UDP_Test/include/utilities.h
@@ -0,0 +1,30 @@
+
+#pragma once
+
+#ifndef Waveshare_ESP32_S3_ETH
+#define Waveshare_ESP32_S3_ETH
+#endif
+
+
+
+#define ETH_MISO_PIN                    12//11
+#define ETH_MOSI_PIN                    11//12
+#define ETH_SCLK_PIN                    13//10
+#define ETH_CS_PIN                      14//9
+#define ETH_INT_PIN                     10//13
+#define ETH_RST_PIN                     9//14
+
+#define ETH_ADDR                        1
+
+#define IR_FILTER_NUM                   -1
+
+
+
+
+
+
+
+
+
+
+
---
+++ SW-ESP/UDP_Test/src/main.cpp
@@ -0,0 +1,69 @@
+#include <WiFi.h>
+#include <AsyncUDP.h>
+#include <ETHClass.h>  // Modified ETHClass for Ethernet support
+#include "utilities.h"
+
+// Ethernet settings
+byte mac[] = {0xDE, 0xAD, 0xBE, 0xEF, 0xFE, 0xED}; // MAC address
+const int udpPort = 12345; // UDP port for broadcasting
+
+static bool eth_connected = false; // Ethernet connection status
+void WiFiEvent(WiFiEvent_t);
+
+AsyncUDP udp;
+
+void setup() {
+  Serial.begin(9600);
+  while (!Serial || millis() < 5000) {
+    ; // wait for serial port to connect. Needed for native USB port only
+  }
+
+  WiFi.onEvent(WiFiEvent);
+  if (!ETH.beginSPI(ETH_MISO_PIN, ETH_MOSI_PIN, ETH_SCLK_PIN, ETH_CS_PIN, ETH_RST_PIN, ETH_INT_PIN)) {
+    printf("Ethernet initialization failed!\n");
+  }
+
+  // Wait for Ethernet connection
+  while (!eth_connected) {
+      printf("Waiting for Ethernet connection...\n");
+      delay(1000);
+  }
+
+  if (udp.connect(IPAddress(192, 168, 1, 100), 1234)) {
+    Serial.println("UDP connected");
+  }
+}
+
+void loop() {
+  udp.broadcastTo("Anyone here?", 1234);
+  delay(5000);
+}
+
+void WiFiEvent(WiFiEvent_t event) {
+  switch (event) {
+    case ARDUINO_EVENT_ETH_START:
+      printf("Ethernet Started\n");
+      ETH.setHostname("esp32-ethernet");
+      break;
+    case ARDUINO_EVENT_ETH_CONNECTED:
+      printf("Ethernet Connected\n");
+      break;
+    case ARDUINO_EVENT_ETH_GOT_IP:
+      printf("Ethernet MAC: %s\n", ETH.macAddress().c_str());
+      printf("IP Address: %s\n", ETH.localIP().toString().c_str());
+      printf("Link Speed: %d Mbps\n", ETH.linkSpeed());
+      printf("Gateway IP: %s\n", ETH.gatewayIP().toString().c_str());
+      eth_connected = true;
+      break;
+    case ARDUINO_EVENT_ETH_DISCONNECTED:
+      printf("Ethernet Disconnected\n");
+      eth_connected = false;
+      break;
+    case ARDUINO_EVENT_ETH_STOP:
+      printf("Ethernet Stopped\n");
+      eth_connected = false;
+      break;
+    default:
+      break;
+    }
+}
