9edc61e27bc623846b7755cd2828f742da4b3a68
Fabian Schaetzschock <fabian@schaetzschock.at>
2025-02-01 17:37:57 +0100

    added comments

--- SW-ESP/globalLibs/FaRiLib/src/espnowSlave.cpp
+++ SW-ESP/globalLibs/FaRiLib/src/espnowSlave.cpp
@@ -2,5 +2,6 @@
 
 bool EspNowSlave::lookForMaster() {
     uint8_t broadcast[] = {0xFF,0xFF,0xFF,0xFF,0xFF,0xFF};
+    //implement message for broadcast
     esp_now_send(broadcast, (uint8_t *) &mySend, sizeof(mySend));
 }
