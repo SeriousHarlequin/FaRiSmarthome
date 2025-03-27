33f350bb13407c0c4adb69f19d3233f763512e1c
Fabian Schaetzschock <fabian@schaetzschock.at>
2025-03-27 19:53:32 +0100

    added Server as docker container

---
+++ SW-Raspberry/start.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+# Start the Flask server in the background
+echo "Starting Flask server..."
+python3 IPReciever/Main.py &
+
+# Start the Nuxt.js server
+cd WebInterface/FaRiWebUI/
+echo "Starting Nuxt.js server..."
+npm run dev -- --host
